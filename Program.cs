bcee7db63ffef4aff16428e7a51dc7dc87199469
infinityworksim-lang <infinityworksim@gmail.com>
2026-07-04 22:32:09 +0300

    Enhance PDF generation with error handling and fonts
    Added error handling for PDF generation and registered custom fonts.

--- Program.cs
+++ Program.cs
@@ -1,10 +1,18 @@
 using MedicalSpace.PdfApi.Models;
 using MedicalSpace.PdfApi.Services;
+using QuestPDF.Drawing;
+using QuestPDF.Helpers;
 using QuestPDF.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
 QuestPDF.Settings.License = LicenseType.Community;
+
+FontManager.RegisterFont(File.OpenRead("Fonts/CairoOblique-8OyDD.otf"));
+FontManager.RegisterFont(File.OpenRead("Fonts/CairoRegular-YqG9j.otf"));
+FontManager.RegisterFont(File.OpenRead("Fonts/Cairo-Bold.ttf"));
+FontManager.RegisterFont(File.OpenRead("Fonts/Cairo-Regular.ttf"));
+
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -16,18 +24,19 @@ var app = builder.Build();
 app.UseSwagger();
 app.UseSwaggerUI();
 
-app.MapPost("/api/prescription",
-(
-    PrescriptionPdfModel model,
-    PrescriptionPdfGenerator generator
-) =>
+app.MapPost("/api/prescription", (PrescriptionPdfModel model, PrescriptionPdfGenerator generator) =>
 {
-    var pdf = generator.GeneratePdf(model);
+	try
+	{
+        var pdf = generator.GeneratePdf(model);
 
-    return Results.File(
-        pdf,
-        "application/pdf",
-        "Prescription.pdf");
+        return Results.File(pdf, "application/pdf", "Prescription.pdf");
+    }
+	catch (Exception ex)
+	{
+        return Results.Text(ex.ToString(), statusCode: 500);
+    }
+   
 });
 
 app.Run();
